d80fa4f36a71537da13e9e055be7bb696c9f34b7
Andi69er <andi.prirsching@gmail.com>
2025-11-25 09:06:37 +0100

    fix: Correct import path for SocketContext

--- frontend/src/components/Lobby.js
+++ frontend/src/components/Lobby.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSocket } from '../context/SocketContext';
+import { useSocket } from '../contexts/SocketContext';
 import gameModes from '../config/gameModes';
 import './Lobby.css';
 
