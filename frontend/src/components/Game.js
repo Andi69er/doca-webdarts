370cc2f2c8816a1790c0d18728782295cfe47eae
Andi69er <andi.prirsching@gmail.com>
2025-12-16 18:19:52 +0100

    TRAE

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1275,6 +1275,7 @@ socket.on('camera-ice', async (data) => {
 
     // Actions
     const handleStartGame = () => {
+        console.log('handleStartGame called');
         if (!socket) return;
         setLocalGameStarted(true);
 
