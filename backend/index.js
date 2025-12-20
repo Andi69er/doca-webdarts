ed8890faee1a220bfe4a14e141e8d8dafc7c26d8
Andi69er <andi.prirsching@gmail.com>
2025-12-20 16:02:21 +0100

    REP23

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,7 @@ app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
 
 // Wildcard-Route für SPA
 app.get(/(.*)/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build', 'ml'));
 });
 
 
